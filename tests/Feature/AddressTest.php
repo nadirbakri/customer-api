4265c555ffbb26d24a4b64fd037bfacb6ac492c3
Nadir B <nadirbakri25@gmail.com>
2022-08-29 13:32:51 +0700

    use refresh database

--- tests/Feature/AddressTest.php
+++ tests/Feature/AddressTest.php
@@ -3,39 +3,28 @@
 namespace Tests\Feature;
 
 use App\Models\Address;
+use App\Models\Customer;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
 class AddressTest extends TestCase
 {
-    public function test_update_data_address()
-    {
-        $response = $this->patch('/api/address/3', [
-            "customer_id" => 4,
-            "address"     => "Jalan Bogor",
-            "district"    => "Tanah Abang",
-            "city"        => "Palembang",
-            "province"    => "Papua",
-            "postal_code" => 121224
-        ]);
-
-        $response->assertStatus(200);
-
-        $address = Address::orderBy('updated_at', 'DESC')->first();
-
-        $this->assertEquals(4, $address->customer_id);
-        $this->assertEquals('Jalan Bogor', $address->address);
-        $this->assertEquals('Tanah Abang', $address->district);
-        $this->assertEquals('Palembang', $address->city);
-        $this->assertEquals('Papua', $address->province);
-        $this->assertEquals(121224, $address->postal_code);
-    }
+    use RefreshDatabase;
 
     public function test_create_new_address()
     {
+        Customer::create([
+            'title'        => 'Tc.',
+            'name'         => 'Test Case',
+            'gender'       => 'T',
+            'phone_number' => '081212121221',
+            'image'        => 'tc.jpg',
+            'email'        => 'tc@test.com'
+        ]);
+
         $response = $this->post('/api/address', [
-            "customer_id" => 4,
+            "customer_id" => 1,
             "address"     => "Jalan Bogor",
             "district"    => "Cilebut",
             "city"        => "Bogor",
@@ -47,12 +36,53 @@ class AddressTest extends TestCase
 
         $address = Address::orderBy('created_at', 'DESC')->first();
 
-        $this->assertEquals(4, $address->customer_id);
+        $this->assertEquals(1, $address->customer_id);
         $this->assertEquals('Jalan Bogor', $address->address);
         $this->assertEquals('Cilebut', $address->district);
         $this->assertEquals('Bogor', $address->city);
         $this->assertEquals('Jawa Barat', $address->province);
         $this->assertEquals(11111, $address->postal_code);
+    }
+
+    public function test_update_data_address()
+    {
+        Customer::create([
+            'title'        => 'Tc.',
+            'name'         => 'Test Case',
+            'gender'       => 'T',
+            'phone_number' => '081212121221',
+            'image'        => 'tc.jpg',
+            'email'        => 'tc@test.com'
+        ]);
+
+        Address::create([
+            "customer_id" => 1,
+            "address"     => "Jalan Bogor",
+            "district"    => "Cilebut",
+            "city"        => "Bogor",
+            "province"    => "Jawa Barat",
+            "postal_code" => 11111
+        ]);
+
+        $response = $this->patch('/api/address/1', [
+            "customer_id" => 1,
+            "address"     => "Jalan Depok",
+            "district"    => "Tanah Abang",
+            "city"        => "Palembang",
+            "province"    => "Papua",
+            "postal_code" => 121224
+        ]);
+
+        $response->assertStatus(200);
+
+        $address = Address::orderBy('updated_at', 'DESC')->first();
+
+        $this->assertEquals(1, $address->customer_id);
+        $this->assertEquals('Jalan Depok', $address->address);
+        $this->assertEquals('Tanah Abang', $address->district);
+        $this->assertEquals('Palembang', $address->city);
+        $this->assertEquals('Papua', $address->province);
+        $this->assertEquals(121224, $address->postal_code);
     }
 
     public function test_failed_to_update_data_address()
@@ -75,7 +105,25 @@ class AddressTest extends TestCase
 
     public function test_delete_data_address()
     {
-        $response = $this->delete('/api/address/7');
+        Customer::create([
+            'title'        => 'Tc.',
+            'name'         => 'Test Case',
+            'gender'       => 'T',
+            'phone_number' => '081212121221',
+            'image'        => 'tc.jpg',
+            'email'        => 'tc@test.com'
+        ]);
+
+        Address::create([
+            "customer_id" => 1,
+            "address"     => "Jalan Bogor",
+            "district"    => "Cilebut",
+            "city"        => "Bogor",
+            "province"    => "Jawa Barat",
+            "postal_code" => 11111
+        ]);
+
+        $response = $this->delete('/api/address/1');
 
         $response->assertStatus(204);
     }
--- tests/Feature/CustomerTest.php
+++ tests/Feature/CustomerTest.php
@@ -4,33 +4,11 @@ namespace Tests\Feature;
 
 use App\Models\Customer;
 use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
 class CustomerTest extends TestCase
 {
-    public function test_update_data_customer()
-    {
-        $response = $this->patch('/api/customer/7', [
-            'title'        => 'Tc.',
-            'name'         => 'Test Case Update',
-            'gender'       => 'T',
-            'phone_number' => '081212121221',
-            'image'        => 'tc.jpg',
-            'email'        => 'tc@test.com'
-        ]);
-
-        $response->assertStatus(200);
-
-        $customer = Customer::orderBy('updated_at', 'DESC')->first();
-
-        $this->assertEquals('Tc.', $customer->title);
-        $this->assertEquals('Test Case Update', $customer->name);
-        $this->assertEquals('T', $customer->gender);
-        $this->assertEquals('081212121221', $customer->phone_number);
-        $this->assertEquals('tc.jpg', $customer->image);
-        $this->assertEquals('tc@test.com', $customer->email);
-    }
+    use RefreshDatabase;
 
     public function test_create_new_customer()
     {
@@ -55,18 +33,50 @@ class CustomerTest extends TestCase
         $this->assertEquals('tc@test.com', $customer->email);
     }
 
-    public function test_failed_to_update_data_customer()
+    public function test_update_data_customer()
     {
+        Customer::create([
+            'title'        => 'Tc.',
+            'name'         => 'Test Case',
+            'gender'       => 'T',
+            'phone_number' => '081212121221',
+            'image'        => 'tc.jpg',
+            'email'        => 'tc@test.com'
+        ]);
+
         $response = $this->patch('/api/customer/1', [
+            'title'        => 'Tc.',
+            'name'         => 'Test Case Update',
+            'gender'       => 'T',
+            'phone_number' => '081212121221',
+            'image'        => 'tc.jpg',
+            'email'        => 'tc@test.com'
+        ]);
+
+        $response->assertStatus(200);
+
+        $customer = Customer::orderBy('updated_at', 'DESC')->first();
+
+        $this->assertEquals('Tc.', $customer->title);
+        $this->assertEquals('Test Case Update', $customer->name);
+        $this->assertEquals('T', $customer->gender);
+        $this->assertEquals('081212121221', $customer->phone_number);
+        $this->assertEquals('tc.jpg', $customer->image);
+        $this->assertEquals('tc@test.com', $customer->email);
+    }
+
+    public function test_failed_to_create_data_customer()
+    {
+        $response = $this->post('/api/customer', [
             'title'        => 'Tc.'
         ]);
 
         $response->assertInvalid(['name', 'gender', 'phone_number','image','email']);
     }
 
-    public function test_failed_to_create_data_customer()
+    public function test_failed_to_update_data_customer()
     {
-        $response = $this->post('/api/customer', [
+        $response = $this->patch('/api/customer/2', [
             'title'        => 'Tc.'
         ]);
 
@@ -82,6 +92,15 @@ class CustomerTest extends TestCase
 
     public function test_get_one_customer()
     {
+        Customer::create([
+            'title'        => 'Tc.',
+            'name'         => 'Test Case',
+            'gender'       => 'T',
+            'phone_number' => '081212121221',
+            'image'        => 'tc.jpg',
+            'email'        => 'tc@test.com'
+        ]);
+
         $response = $this->get('/api/customer/1');
 
         $response->assertStatus(200);
@@ -89,7 +108,16 @@ class CustomerTest extends TestCase
 
     public function test_delete_data_customer()
     {
-        $response = $this->delete('/api/customer/10');
+        Customer::create([
+            'title'        => 'Tc.',
+            'name'         => 'Test Case',
+            'gender'       => 'T',
+            'phone_number' => '081212121221',
+            'image'        => 'tc.jpg',
+            'email'        => 'tc@test.com'
+        ]);
+
+        $response = $this->delete('/api/customer/1');
 
         $response->assertStatus(204);
     }
